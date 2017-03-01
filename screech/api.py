17f5e496424acbd9500561870f83cd54aeda0b08
Marty McGuire <schmartissimo@gmail.com>
2017-03-01 17:59:25 -0500

    micropub proxy
    * support passing through files
    * support multiple values per key
    * interpret response and redirect to Location on 201,202

--- screech/api.py
+++ screech/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, session
+from flask import Blueprint, request, session, redirect
 from .auth import login_required
 import requests
 
@@ -9,18 +9,35 @@ api = Blueprint('api', __name__)
 def publish():
     endpoint = session.get('_micropub_endpoint')
 
-    # TODO: handle passing along file uploads
+    # pass along file uploads, if present
+    files = {}
+    for file_key in ['audio','photo']:
+      if file_key in request.files:
+        f = request.files[file_key]
+        if f.filename != '':
+          # TODO: if media endpoint, upload there and replace value w/ URL
+          # otherwise, pass the file along to the micropub endpoint
+          files[file_key] = (f.filename, f, f.mimetype)
 
-    # TODO: data validation
-    data = request.form.to_dict()
+    # TODO: data validation?
+
+    # iterate over keys to allow multiple values from Flask multidict
+    data = {}
+    for k in request.form.keys():
+      data[k] = request.form.getlist(k)
 
     headers = { 'Authorization': "Bearer %s" % session.get('_micropub_access_token') }
-
-    return "%s" % ((endpoint, data, headers), )
 
     r = requests.post(
       endpoint,
       data=data,
-      headers=headers
+      headers=headers,
+      files=files
     )
-    return "WHAT'S UP BUTTERCUP %s" % r.text
+
+    # check for a 201 or 202 and Location: header for success
+    # redirect to Location!
+    if (r.status_code == requests.codes.created) or (r.status_code == request.codes.accepted):
+      return redirect(r.headers.get('location'))
+    else:
+      return "Micropub endpoint did not return a Location. %s" % r.text
